4bee2da35cd8cd64c3ee6a12218d8e30cda2dc78
tomalbrc <tomalbrc@me.com>
2019-09-26 22:30:21 +0200

    More example/test code

--- PDF-Plus-X/main.cpp
+++ PDF-Plus-X/main.cpp
@@ -14,6 +14,31 @@
 #include "Object.hpp"
 #include "Catalog.hpp"
 
+void make_table(PDF_Plus::Page *page)
+{
+	int hc = 7;
+	int h = 40;
+	
+	int wc = 6;
+	int w = 90;
+	for (int i = 1; i <= wc; i++) {
+		page->contents()->drawLine(i*w,h,		i*w,h*hc);
+	}
+	
+	for (int i = 1; i <= hc; i++) {
+		page->contents()->drawLine(w,i*h,		w*wc,i*h);
+	}
+	
+	for (int i = 1; i <= wc-1; i++) {
+		for (int j = 1; j <= hc-1; j++) {
+			page->contents()->drawText(std::to_string(i)+" "+std::to_string(hc-j),
+									   i*w+10,//x
+									   j*h+10,//y
+									   12);//font
+		}
+	}
+}
+
 int main(int argc, const char * argv[])
 {
 	using namespace PDF_Plus;
@@ -22,14 +47,16 @@ int main(int argc, const char * argv[])
 	
 	auto page = std::make_shared<Page>(doc.get());
 	page->contents()->drawText("Page 1", 10, 10, 10);
-	page->contents()->drawText(") endstream Title..?", 100, 700, 30);
 	page->contents()->drawText("Hello, world!", 100, 300, 30);
-	page->contents()->drawRect(10, 10, 110, 110);
+	
+	make_table(page.get());
+	
 	doc->addPage(page);
 	
 	auto page2 = std::make_shared<Page>(doc.get());
 	page2->contents()->drawText("Page 2", 10, 10, 10);
 	page2->contents()->drawLine(0,0, 500,800);
+	page2->contents()->drawRect(10, 10, 110, 110);
 	doc->addPage(page2);
 	
 	auto page3 = std::make_shared<Page>(doc.get());
