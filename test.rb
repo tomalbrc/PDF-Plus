289879011c3ba4d013b477604b500fedbcae02ef
tomalbrc <tomalbrc@me.com>
2019-09-28 02:17:14 +0200

    Update preflight

--- test.rb
+++ test.rb
@@ -1,10 +1,6 @@
 require "preflight"
 
-preflight = Preflight::Profiles::PDFX1A.new
+preflight = Preflight::Profiles::PDFA1A.new
 
 puts preflight.check("/tmp/pdfplus.pdf").inspect
-
-File.open("somefile.pdf", "rb") do |file|
-  puts preflight.check(file).inspect
-end
 
