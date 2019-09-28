9537a63a80e03b28c5a944eb98b0220bf9fa3a67
tomalbrc <tomalbrc@me.com>
2019-09-29 00:20:29 +0200

    Added representation for String

---
+++ PDF-Plus-X/String.hpp
@@ -0,0 +1,49 @@
+//
+//  String.hpp
+//  PDF-Plus-X
+//
+//  Created by Tom Albrecht on 28.09.19.
+//  Copyright © 2019 Tom Albrecht. All rights reserved.
+//
+
+#ifndef String_hpp
+#define String_hpp
+
+#include <stdio.h>
+
+#include <cstdio>
+#include <string>
+#include <regex>
+#include <iostream>
+
+class String {
+public:
+	String() = default;
+	String(const std::string& data) : _data{data} {}
+	String(std::string&& data) : _data{std::move(data)} {}
+	
+	auto operator==(const String& rhs) {
+		_data = rhs._data;
+	}
+	
+	auto operator==(String&& rhs) {
+		_data = std::move(rhs._data);
+	}
+	
+	friend std::ostream& operator<<(std::ostream& out, const String& str) {
+		return out << '(' << str.escape(str._data) << ')' << '\n';
+	}
+	
+private:
+	std::string _data;
+	
+	std::string escape(const std::string& text) const
+	{
+		auto cpy = text;
+		cpy = std::regex_replace(cpy, std::regex(R"(\\)"), R"(\\)");
+		cpy = std::regex_replace(cpy, std::regex(R"(\()"), R"(\()");
+		return std::regex_replace(cpy, std::regex(R"(\))"), R"(\))");
+	}
+};
+
+#endif /* String_hpp */
