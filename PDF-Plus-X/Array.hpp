276feba84c2494a014cdcabb76fa3ed509f26afe
tomalbrc <tomalbrc@me.com>
2019-09-29 21:39:07 +0200

    Initializer list & comments for Array

--- PDF-Plus-X/Array.hpp
+++ PDF-Plus-X/Array.hpp
@@ -28,53 +28,60 @@ namespace PDF_Plus {
 		/**
 	
 		 */
-		Array(Ts... args) : _data{args...} {}
-	
+		Array(Ts... args) : Array{args...} {}
+		
 		/**
-	
+		 
 		 */
-		Array(const std::vector<Value_t>& data) : _data{data} {}
-	
+		Array(const std::initializer_list<Value_t>& data) : _data{data} {}
+		Array(std::initializer_list<Value_t>&& data) : _data{std::move(data)} {}
+		
 		/**
-	
+		 Appends item at (array.size())
 		 */
-		Array(std::vector<Value_t>&& data) : _data{std::move(data)} {}
-	
-		/**
-	
-		 */
-		void append(const Value_t& item) {
-			_data.emplace_back(item);
+		auto append(const Value_t& item) {
+			return _data.emplace_back(item);
 		}
-	
+		
 		/**
-	
+		 Insert item at index
+		 */
+		auto insert(const Value_t& item, const std::size_t& idx = 0) {
+			return _data.insert(_data.begin() + idx, item);
+		}
+		
+		/**
+		 Remove item from Array
 		 */
 		void remove(const Value_t& item) {
 			_data.erase(std::remove(_data.begin(), _data.end(), item), _data.end());
 		}
 	
 		/**
-	
+		 Array size
 		 */
 		auto size() const {
 			return _data.size();
 		}
 	
 		/**
-	
+		 Array empty
 		 */
 		auto empty() const {
 			return _data.empty();
 		}
-	
+		
 		/**
-	
+		 Set value to item. Can be of the ones specified in Ts
 		 */
 		Value_t& set(const std::size_t& idx) {
 			return _data.at(idx);
 		}
-	
+		
+		/**
+		 Will throw if template parameter Get_t
+		 does not match with the type of the item at idx
+		 */
 		template <class Get_t>
 		Get_t& get(const std::size_t& idx) {
 			static_assert(VariantHelper::isVariantMember<Get_t, 	std::variant<Ts...>>::value, "type not in list");
