45ed7fe3fc166e4f3a364b4ce5d98f120cfc7900
tomalbrc <tomalbrc@me.com>
2019-09-29 11:12:08 +0200

    Header for Key constants

---
+++ PDF-Plus-X/Key.h
@@ -0,0 +1,31 @@
+//
+//  Keys.h
+//  PDF-Plus-X
+//
+//  Created by Tom Albrecht on 29.09.19.
+//  Copyright © 2019 Tom Albrecht. All rights reserved.
+//
+
+#ifndef Keys_h
+#define Keys_h
+
+#define QUOTE(name) #name
+#define STR(macro) QUOTE(macro)
+#define STR_KEY(x) const std::string x = STR(x)
+
+/**
+ A place to store all possible keys for Dictionaries
+ */
+namespace PDF_Plus::Key {
+	// General
+	STR_KEY(Type);
+	STR_KEY(Length);
+
+	// Page(s)
+	STR_KEY(MediaBox);
+	STR_KEY(TrimBox);
+	
+	STR_KEY(Font);
+}
+
+#endif /* Keys_h */
