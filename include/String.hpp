5a05e0b39e88d8bfe756c29e9dcee99674487623
tomalbrc <tomalbrc@me.com>
2019-10-03 01:07:10 +0200

    Add String to namespace

--- include/String.hpp
+++ include/String.hpp
@@ -16,48 +16,50 @@
 #include <regex>
 #include <iostream>
 
-/**
- 
- */
-class String {
-public:
+namespace PDF_Plus {
 	/**
 	 
 	 */
-	String() = default;
-	
-	/**
-	 
-	 */
-	String(const std::string& data) : _data{data} {}
-	
-	/**
-	 
-	 */
-	String(std::string&& data) : _data{std::move(data)} {}
-	
-	auto operator==(const String& rhs) {
-		return rhs._data == _data;
-	}
-	
-	/**
-	 
-	 */
-	friend std::ostream& operator<<(std::ostream& out, const String& str) {
-		return out << '(' << str.escape(str._data) << ')';
-	}
-	
-private:
-	std::string _data;
-	
-	///
-	std::string escape(const std::string& text) const
-	{
+	class String {
+	public:
+		/**
+		 
+		 */
+		String() = default;
+		
+		/**
+		 
+		 */
+		String(const std::string& data) : _data{data} {}
+		
+		/**
+		 
+		 */
+		String(std::string&& data) : _data{std::move(data)} {}
+		
+		auto operator==(const String& rhs) {
+			return rhs._data == _data;
+		}
+		
+		/**
+		 
+		 */
+		friend std::ostream& operator<<(std::ostream& out, const String& str) {
+			return out << '(' << str.escape(str._data) << ')';
+		}
+		
+	private:
+		std::string _data;
+		
+		///
+		std::string escape(const std::string& text) const
+		{
 		auto cpy = text;
 		cpy = std::regex_replace(cpy, std::regex(R"(\\)"), R"(\\)");
 		cpy = std::regex_replace(cpy, std::regex(R"(\()"), R"(\()");
 		return std::regex_replace(cpy, std::regex(R"(\))"), R"(\))");
-	}
-};
+		}
+	};
+}
 
 #endif /* String_hpp */
